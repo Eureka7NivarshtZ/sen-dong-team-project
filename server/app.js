63a24c38b1419c0c72950ff4e43561c2ba953da3
Khoi <khoidang01234@gmail.com>
2026-06-04 20:38:44 +0700

    fix deploy

--- server/app.js
+++ server/app.js
@@ -22,7 +22,12 @@ const adminChamSocRoutes = require("./routes/adminChamSoc.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
